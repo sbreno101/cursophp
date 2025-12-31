fb19d9657510f920c3b66b83b98945e87cf9dd8f
sbreno001 <sbreno.silva03@gmail.com>
2025-12-31 13:39:26 -0300

    .

---
+++ desafios/d001/resposta.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Resultado Final</h1>
+        <p>
+            <?php 
+                $n = $_REQUEST["num"] ?: 0;
+                $a = $n - 1;
+                $s = $n + 1;
+                print "O número escrito foi $n";
+                print "<br>";
+                print "O seu antecessor é $a";
+                print "<br>";
+                print "O seu sucessor é $s";
+            ?>
+        </p>
+    </main>
+</body>
+</html>
